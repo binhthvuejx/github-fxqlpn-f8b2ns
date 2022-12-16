20c2c137862ca89a25b3349b523b7df4bda5c18a
binhthvuejx <binhth.vuejx@gmail.com>
2022-12-16 12:47:24 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-fxqlpn-f8b2ns
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-fxqlpn-vhwbyh)
---
+++ src/components/CounterVisible.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="counter-visible">
+    <button @click="subtract()">-</button>
+    <pre>{{ count }}</pre>
+    <button @click="add()">+</button>
+  </div>
+  <div class="counter-visible-message">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const count = ref(0);
+    const add = () => (count.value = count.value + 1);
+    const subtract = () => (count.value = count.value - 1);
+
+    return {
+      count,
+      add,
+      subtract,
+    };
+  },
+};
+</script>
+
+<style>
+.counter-visible {
+  display: grid;
+  font-size: 2em;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  margin-top: 2em;
+  place-items: center;
+}
+.counter-visible-message {
+  text-align: center;
+}
+</style>
